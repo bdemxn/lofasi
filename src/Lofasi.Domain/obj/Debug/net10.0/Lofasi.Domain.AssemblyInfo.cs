4c45d6adebbb109d89786ce8ce3aed29556bf8a9
kevin bonilla <kb1909zzz@gmail.com>
2026-07-15 22:12:23 -0600

    responsabilities scaffolding

--- src/Lofasi.Domain/obj/Debug/net10.0/Lofasi.Domain.AssemblyInfo.cs
+++ src/Lofasi.Domain/obj/Debug/net10.0/Lofasi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lofasi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3666cea9166821921b9c98658c727b25ac6788fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b5af637d923256a28b9a388ef4a27f18aa0fbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lofasi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lofasi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
